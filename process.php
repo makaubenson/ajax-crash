3350abc8ff8eb837c8ee085503789a0004b42f2b
Benson <bensonmakau2000@gmail.com>
2022-02-19 20:06:20 +0300

    post request

--- process.php
+++ process.php
@@ -1,6 +1,11 @@
 <?php
 
 echo 'processing....';
+
+//check for POST variable
+if(isset($_POST['name'])){
+    echo 'POST: Your name is '.$_POST['name'];
+}
 //check for GET variable
 if(isset($_GET['name'])){
     echo 'GET: Your name is '.$_GET['name'];
