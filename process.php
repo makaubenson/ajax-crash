8a9c0ab8067a692783928d16cdc2a86e768e0725
Benson <bensonmakau2000@gmail.com>
2022-02-20 01:05:45 +0300

    added data to db using ajax post request

--- process.php
+++ process.php
@@ -1,10 +1,19 @@
 <?php
 
+$conn = mysqli_connect('localhost','benson','benson','ajaxtest');
 echo 'processing....';
 
 //check for POST variable
 if(isset($_POST['name'])){
-    echo 'POST: Your name is '.$_POST['name'];
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+  //  echo 'POST: Your name is '.$_POST['name'];
+
+    $query = "INSERT INTO users(name) VALUES('$name')";
+    if(mysqli_query($conn, $query)){
+        echo 'User Added...';
+    }else{
+        echo 'ERROR: '.mysqli_error($conn);
+    }
 }
 //check for GET variable
 if(isset($_GET['name'])){
